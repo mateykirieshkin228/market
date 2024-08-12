734e543d48edd1acb5308c00368965ca6b38c276
mateykirieshkin228 <matvejmaksimov699@gmail.com>
2024-08-12 20:43:02 +0300

    nothing important

--- js/jsmain.js
+++ js/jsmain.js
@@ -1,13 +1,14 @@
 class MyElement extends HTMLElement {
-            constructor(name, image, price) {
+            constructor(name, image, price, number) {
                 super();
                 this.name = name;
                 this.image = image;
                 this.price = price;
+                this.number = number;
             }
             
             connectedCallback() {
-                this.innerHTML = `<div class="product">
+                this.innerHTML = `<div onclick.window.open(${this.number}.html) class="product">
         <img src="${this.image}" id="image">
         <p id="name">${this.name}</p>
         <p id="price">${this.price}</p>
