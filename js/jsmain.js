608bbcef343c92b407b92eea8c4b7f6c513e56ca
mateykirieshkin228 <matvejmaksimov699@gmail.com>
2024-08-13 21:57:17 +0300

    nothing important

--- js/jsmain.js
+++ js/jsmain.js
@@ -1,20 +1,25 @@
 class MyElement extends HTMLElement {
-            constructor(name, image, price, number) {
+            constructor(name, image, price) {
                 super();
                 this.name = name;
                 this.image = image;
                 this.price = price;
-                this.number = number;
             }
             
             connectedCallback() {
-                this.innerHTML = `<div onclick.window.open(${this.number}.html) class="product">
+                this.innerHTML = `<div class="product">
         <img src="${this.image}" id="image">
         <p id="name">${this.name}</p>
         <p id="price">${this.price}</p>
-    </div>`;
-            }
-        }
+    </div>`
+            const productCards = document.querySelectorAll('.product-card');
+
+            productCards.forEach((card, index) => {
+            card.addEventListener('click', () => {
+            if (index === 0) {window.open('products/firstprod.html', '_blank');}
+        });
+    });
+}};
         customElements.define("product-card",MyElement);
 
 
